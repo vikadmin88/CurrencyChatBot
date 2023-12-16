faf055f1abb3ef026a711063ecadf79faf6b177b
kolobo4ok <142622191+kolobo4ok@users.noreply.github.com>
2023-12-16 23:23:02 +0200

    Create CurrencyHolder

---
+++ src/main/java/org/javacrafters/banking/CurrencyHolder.java
@@ -0,0 +1,39 @@
+package org.javacrafters.banking;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class CurrencyHolder {
+
+
+    private static Map<String, Map<String, NormalizeCurrencyPair>> currency = new HashMap<>();
+
+
+    public static void refreshRates() {
+        /*TODO
+       Заглушка для методу оновлення курсів валют логіка оновлення курсів валют тут refreshRates() пока оставить
+       как заглушку в цикле запрашивает каждый банк на получение валют, делает запрос вида: на объекте банка bankObj.getRates()
+       от банка получает коллекцию Map<String, NormalizeCurrencyPair> и добавляет add() ее в свою коллекцию с ключем имени банка
+       который получит из bankObj.getLocalName()
+         */
+        /* можливо так?
+        for (Bank bankObj : getAllBanks()) {
+            Map<String, NormalizeCurrencyPair> rates = bankObj.getRates();
+            currency.put(bankObj.getLocalName());
+        }
+        TODO bankObj
+        */
+    }
+
+//    private static void add(String bankName, Map<String, NormalizeCurrencyPair> rates) {
+//        currency.put(bankName, rates);
+//    } not use??? 16/12
+
+    public static Map<String, Map<String, NormalizeCurrencyPair>> getRates() {
+        return currency;
+    }
+
+    public static Map<String, NormalizeCurrencyPair> getRates(String bankName) {
+        return currency.get(bankName);
+    }
+}
