77eb9ff3c1d27bf189a4d992656b4eb5a45ab85b
Viktor Kudryavtsev <viktork8888@gmail.com>
2023-12-16 15:57:40 +0200

    fix minor

--- src/main/java/org/javacrafters/AppLauncher.java
+++ src/main/java/org/javacrafters/AppLauncher.java
@@ -1,12 +1,10 @@
 package org.javacrafters;
 
 import org.javacrafters.core.ChatBot;
-import org.javacrafters.scheduler.Scheduler;
 import org.javacrafters.user.User;
 import org.javacrafters.core.ConfigLoader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ScheduledFuture;
 
 
 public class AppLauncher {
@@ -21,21 +19,6 @@ public class AppLauncher {
         String botToken = ConfigLoader.get("botToken");
 
         ChatBot bot = new ChatBot(users, appName, botName, botToken);
-
-//        User user = new User(1L, "Viktest1", "UserName1");
-//        ScheduledFuture<?> notifyTask = new Scheduler().schedule(bot, user, 9);
-
-//        user.setScheduledTask(notifyTask);
-//        System.out.println("user = " + user);
-//        System.out.println("user.getScheduledTask() = " + user.getScheduledTask());
-//        System.out.println("user.getScheduledTask().cancel(true) = " + user.getScheduledTask().cancel(true));
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        System.out.println("notifyTask.isDone() = " + notifyTask.isDone());
-
         bot.botRun();
     }
 
