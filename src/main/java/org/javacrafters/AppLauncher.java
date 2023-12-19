623a513916d57535550bfd0da0f103417000e8ee
Viktor Kudryavtsev <viktork8888@gmail.com>
2023-12-20 01:55:18 +0200

    refactoring

--- src/main/java/org/javacrafters/AppLauncher.java
+++ src/main/java/org/javacrafters/AppLauncher.java
@@ -7,6 +7,7 @@ import org.javacrafters.core.AppRegistry;
 import org.javacrafters.core.ChatBot;
 import org.javacrafters.core.UserLoader;
 import org.javacrafters.core.storage.JsonStorageProvider;
+import org.javacrafters.networkclient.NetworkClient;
 import org.javacrafters.networkclient.NetworkStreamReader;
 import org.javacrafters.core.ConfigLoader;
 
@@ -18,10 +19,9 @@ public class AppLauncher {
         System.out.println("Program starting in Thread: " + Thread.currentThread().getName());
 
         AppRegistry.initDefaults();
-        AppRegistry.setNetClient(new NetworkStreamReader());
-        AppRegistry.addBank("PB", new PrivatBank());
-        AppRegistry.addBank("MB", new MonoBank());
-        AppRegistry.addBank("NBU", new NbuBank());
+        AppRegistry.addBank("PB", new PrivatBank(ConfigLoader.get("BANK_PB_API_URL"), new NetworkStreamReader()));
+        AppRegistry.addBank("MB", new MonoBank(ConfigLoader.get("BANK_MB_API_URL"), new NetworkStreamReader()));
+        AppRegistry.addBank("NBU", new NbuBank(ConfigLoader.get("BANK_NBU_API_URL"), new NetworkStreamReader()));
 
         UserLoader.setStorageProvider(new JsonStorageProvider()).load();
 
